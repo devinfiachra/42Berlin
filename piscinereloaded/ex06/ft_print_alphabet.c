bca49375621eedd346abb27d45baabb644dfa43b
Devin Fiachra Duffy Meade <dduffy-m@c4b7c3.42berlin.de>
2023-11-09 12:15:57 +0100

    add first exercises

---
+++ piscinereloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduffy-m <dduffy-m@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 16:45:21 by dduffy-m          #+#    #+#             */
+/*   Updated: 2023/11/08 16:48:49 by dduffy-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		ft_putchar(alpha);
+		alpha++;
+	}
+}
