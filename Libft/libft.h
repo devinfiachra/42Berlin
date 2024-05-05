21b9296708b51651ed2a79dd1c120c3cc836ec33
Devin Fiachra Duffy Meade <dduffy-m@c4b10c3.42berlin.de>
2024-05-05 19:17:16 +0200

    add placeholder functions to header

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dduffy-m <dduffy-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:24:16 by dduffy-m          #+#    #+#             */
-/*   Updated: 2024/05/05 17:37:57 by dduffy-m         ###   ########.fr       */
+/*   Updated: 2024/05/05 18:55:13 by dduffy-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+//C LIBRARY FUNCTIONS
 int			ft_atoi(const char *str);
-//ft_bzero()//
-//ft_calloc()//
+//void 		ft_bzero(void *s, size_t n);
+//void *ft_calloc(size_t nmemb, size_t size);
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int arg);
-//ft_memchr()//
-//ft_memcmp()//
-//ft_memcpy()//
-//ft_memmove()//
-//ft_memset()//
+//void *ft_memchr(const void *s, int c, size_t n);
+//int ft_memcmp(const void *s1, const void *s2, size_t n);
+//void *ft_memcpy(void *dest, const void *src, size_t n);
+//void *ft_memmove(void *dest, const void *src, size_t n);
+//void *ft_memset(void *s, int c, size_t n);
 char		*ft_strchr(const char *str, int c);
-//ft_strdup()//
-//ft_strlcat()//
-//ft_strlcpy()//
+//char *ft_strdup(const char *s);
+//ft_strlcat(char *dst, const char *src, size_t size);
+//ft_strlcpy(char *dst, const char *src, size_t size);
 size_t		ft_strlen(const char *str);
 int			ft_strncmp(const char *str1, const char *str2, size_t n);
-//ft_strnstr()//
+//char *ft_strnstr(const char *big, const char *little, size_t len);
 char		*ft_strrchr(const char *str, int c);
 int			ft_toupper(int ch);
 int			ft_tolower(int ch);
 
+// ADDITIONAL FUNCTIONS//
 #endif
 
 
